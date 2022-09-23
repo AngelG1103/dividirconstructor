4a87569157bb0e636a4a092a2805d7e3b0928e47
Angel González <anngeeldavid@gmail.com>
2022-09-22 20:11:37 -0600

    Agregar archivos de proyecto.

---
+++ dividirconstructor/dividir.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dividirconstructor
+{
+    class dividir
+    {
+        public double V1;
+        public double V2;
+
+        public dividir()
+        {
+        }
+
+        public dividir(double v1, double v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+        public double divide()
+        {
+            return V1 / V2;
+        }
+    }
+}
